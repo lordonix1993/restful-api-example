e6d9ae16b9c0126e4cc7d6cf1173119a679976d8
Alexandra Goloskok <alexandra.goloskok_new@anstrex.com>
2023-01-03 15:39:23 +0200

    Changed Me and Registration tests and removed constants from TestCase

--- tests/Feature/Api/Auth/MeTest.php
+++ tests/Feature/Api/Auth/MeTest.php
@@ -15,29 +15,6 @@ class MeTest extends TestCase
     private mixed $response_login_arr = '';
 
     /**
-     * This method run every time before run every test method
-     *
-     * @return void
-     */
-    public function setUp(): void {
-        parent::setUp();
-
-        $password = Str::random(10);
-        $this->user = User::factory()
-            ->set('password', bcrypt($password))
-            ->create();
-
-        $response_login = $this->post('/api/auth/login', [
-            'email'     => $this->user['email'],
-            'password'  => $password
-        ]);
-
-        try {
-            $this->response_login_arr = $response_login->decodeResponseJson()->json();
-        } catch(\Throwable $err) {}
-    }
-
-    /**
      * The test checks response where don't set token
      * This request must return 401 code
      *
@@ -63,7 +40,20 @@ class MeTest extends TestCase
      */
     public function test_me_get_response_with_successful_token(): void
     {
-        $this->assertNotEmpty($this->response_login_arr);
+        $password = Str::random(10);
+        $this->user = User::factory()
+            ->set('password', bcrypt($password))
+            ->create();
+
+        $response_login = $this->post('/api/auth/login', [
+            'email'     => $this->user['email'],
+            'password'  => $password
+        ]);
+
+        try {
+            $this->response_login_arr = $response_login->decodeResponseJson()->json();
+        } catch(\Throwable $err) {}
+
         $token = '';
 
         if(
@@ -96,35 +86,17 @@ class MeTest extends TestCase
      */
     public function test_me_get_response_with_not_correctly_token(): void
     {
-        $this->assertNotEmpty($this->response_login_arr);
-        $token = '';
+        $response = $this->withHeaders([
+            "Authorization" => "Bearer ".Str::random(150)
+        ])->post('/api/auth/me');
 
-        if(
-            isset($this->response_login_arr['data']) &&
-            isset($this->response_login_arr['data']['access_token'])
-        ) {
-            $token = $this->response_login_arr['data']['access_token'];
-
-            $response = $this->withHeaders([
-                "Authorization" => "Bearer ".$token.'1'
-            ])->post('/api/auth/me');
-
-            $response->assertStatus(self::HTTP_CODE_UNAUTHORIZED)
-                ->assertJson([
-                    'success' => false,
-                    'message' => __('auth.response.401.middleware')
-                ])
-                ->assertJsonStructure([
-                    'data' => []
-                ]);
-        }
-
-        $this->assertNotEmpty($token);
-    }
-
-    public function tearDown(): void
-    {
-        $this->response_login_arr = '';
-        parent::tearDown();
+        $response->assertStatus(self::HTTP_CODE_UNAUTHORIZED)
+            ->assertJson([
+                'success' => false,
+                'message' => __('auth.response.401.middleware')
+            ])
+            ->assertJsonStructure([
+                'data' => []
+            ]);
     }
 }
